2854bcbc883ff4c2762365183aa5af12e2f2f830
ppedro <ppedro.o.verissimo@gmail.com>
2024-01-23 17:38:27 -0300

    feat: slash commands

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ const client = new Client({
     ],
 });
 
-client.on('ready', (c) => {
-    console.log(`${c.user.username} is online.`);
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    };
 });
 
 client.login(process.env.TOKEN);
